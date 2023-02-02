9926261ab19707da1120c454d244b1f81c45e4ac
ABiviano13 <abiviano0@outlook.com>
2023-02-02 15:45:47 +0100

    FEAT js multples

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 //CREARE UN CICLO FOR
+
 //1. Scrivere formula del ciclo for, con variabile contatore, condizione e incremento
-  //- stampare con console.log numeri da 1 a 100
-  //- far si che i multipli di 15 vengono chiamati FizzBuzz
-  //- far si che i multipli di 5 vengono chiamati Buzz
-  //-far si che i multipli di 3 vengono chiamati Fizz
+for (let i = 0; i < 100; i++) {
+    //- stampare con console.log numeri da 1 a 100
+    console.log (i + 1);
+
+    //- far si che i multipli di 15 vengono chiamati FizzBuzz
+    let multiples15 = (i + 1) % 15;
+    //- far si che i multipli di 5 vengono chiamati Buzz
+    let multiples5 = (i + 1) % 5;
+    //-far si che i multipli di 3 vengono chiamati Fizz
+    let multiples3 = (i + 1) % 3;
+
+    // Stampare con console.log i multipli con le denominazioni distintive
+    if (multiples15 === 0) {
+        console.log (multiples15, 'FizzBuzz');
+	} else if (multiples5 === 0){
+        console.log (multiples5, 'Buzz');
+    } else if (multiples3 === 0) {
+        console.log (multiples3, 'Fizz');
+	};
+};
+  
