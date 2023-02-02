9b2d1053920885a119cca2360c2cca6fc85f1b6f
ABiviano13 <abiviano0@outlook.com>
2023-02-02 15:21:46 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//CREARE UN CICLO FOR
+//1. Scrivere formula del ciclo for, con variabile contatore, condizione e incremento
+  //- stampare con console.log numeri da 1 a 100
+  //- far si che i multipli di 15 vengono chiamati FizzBuzz
+  //- far si che i multipli di 5 vengono chiamati Buzz
+  //-far si che i multipli di 3 vengono chiamati Fizz
